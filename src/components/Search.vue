7f273bdac5e4d41384ca8c10003c1f023ddbf5f1
Andika Bayu Santoso <andika.bayu.s2001@gmail.com>
2022-01-05 23:15:27 +0700

    Derp

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -5,7 +5,7 @@
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 opacity-30 text-gray-100" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                 </svg>
-                <input class="bg-gray-900 text-gray-100 outline-none" v-model="search" type="text" placeholder="Cari Komik" @keyup.enter="this.$router.replace({name: 'manga.search', params:{ key: this.search}}); $forceUpdate" />
+                <input class="bg-gray-900 text-gray-100 outline-none" v-model="search" type="text" placeholder="Cari Komik" @keyup.enter="this.$router.push({name: 'manga.search', params:{ key: this.search}}); $forceUpdate" />
             </div>
         </div>
     </div>
